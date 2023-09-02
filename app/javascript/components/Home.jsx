29791445359069ca92fbcecd8a5ed1adb59c554b
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2023-09-02 14:08:06 -0700

    Created project.

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+      </div>
+    </div>
+  </div>
+);
